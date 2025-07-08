dccbbf345e175cfce8c5e943cd95b69fe91ee23e
icykirby <jamericanmadison@gmail.com>
2025-07-07 22:00:29 -0400

    base template

---
+++ index.js
@@ -0,0 +1,19 @@
+//navigation
+const navSelect = document.querySelectorAll('.navSelect');
+const contentSections = document.querySelectorAll('.content'); 
+
+navSelect.forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+      
+        const clickedSection = this.getAttribute('data-section');
+        
+        navSelect.forEach(n => n.classList.remove('active'));
+        contentSections.forEach(c => c.classList.remove('active')); 
+        
+        this.classList.add('active');
+        document.getElementById(clickedSection).classList.add('active');
+    });
+});
+
+
