5267f97c5225c4714c0254352bec0bee2ff41ff7
icykirby <jamericanmadison@gmail.com>
2025-07-14 20:12:21 -0400

    changed the ui layout, added a pfp/settings option

--- index.js
+++ index.js
@@ -16,4 +16,19 @@ navSelect.forEach(link => {
     });
 });
 
-
+//dropdown button
+function dropdownMenu() {
+    document.getElementById("dropdownMenu").classList.toggle("show");
+}
+window.onclick = function(event) {
+  if (!event.target.matches('.dropdownBtn')) {
+    let dropdowns = document.getElementsByClassName("dropdownContent");
+    let i;
+    for (i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if(openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
